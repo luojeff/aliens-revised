ca5c3aad5f3f8d8b6e620ee1eef84da2e9842e7e
Jason Kao <jasonkao85@gmail.com>
2017-12-20 00:06:40 -0500

    all_flights append

--- scripts/market_to_loc.py
+++ scripts/market_to_loc.py
@@ -6,19 +6,24 @@ cml = ast.literal_eval(
     open('../data/lookups/l_city_market_id.dict', 'r').read()
 )
 
-flights = pd.read_csv('../data/flights/fl_2013-01.csv')
-flights = flights[
-    pd.notnull(flights['ARR_TIME']) &
-    pd.notnull(flights['DEP_TIME']) &
-    pd.notnull(flights['FL_DATE']) &
-    (flights['ORIGIN_CITY_MARKET_ID'].isin(cml.keys())) &
-    (flights['DEST_CITY_MARKET_ID'].isin(cml.keys()))
-]
-flights['o_lat'] = flights['ORIGIN_CITY_MARKET_ID'].apply(lambda x: cml[x]['lat'])
-flights['o_lng'] = flights['ORIGIN_CITY_MARKET_ID'].apply(lambda x: cml[x]['lng'])
-flights['d_lat'] = flights['DEST_CITY_MARKET_ID'].apply(lambda x: cml[x]['lat'])
-flights['d_lng'] = flights['DEST_CITY_MARKET_ID'].apply(lambda x: cml[x]['lng'])
+all_flights = pd.DataFrame()
 
-print flights.head()
+for i in range(1, 9):
+    flights = pd.read_csv('../data/flights/fl_2013-0%d.csv' % i)
+    flights = flights[
+        pd.notnull(flights['ARR_TIME']) &
+        pd.notnull(flights['DEP_TIME']) &
+        pd.notnull(flights['FL_DATE']) &
+        (flights['ORIGIN_CITY_MARKET_ID'].isin(cml.keys())) &
+        (flights['DEST_CITY_MARKET_ID'].isin(cml.keys()))
+    ]
+    flights['o_lat'] = flights['ORIGIN_CITY_MARKET_ID'].apply(lambda x: cml[x]['lat'])
+    flights['o_lng'] = flights['ORIGIN_CITY_MARKET_ID'].apply(lambda x: cml[x]['lng'])
+    flights['d_lat'] = flights['DEST_CITY_MARKET_ID'].apply(lambda x: cml[x]['lat'])
+    flights['d_lng'] = flights['DEST_CITY_MARKET_ID'].apply(lambda x: cml[x]['lng'])
+
+    all_flights = all_flights.append(flights, ignore_index=True)
+
+all_flights.to_csv('all_flights.csv')
 
 
